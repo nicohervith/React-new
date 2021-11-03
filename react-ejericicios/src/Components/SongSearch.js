194519e86430b78ff724c053e9bc2cff108c63e1
nicohervith <niko_hervith@hotmail.com>
2021-11-03 17:21:45 -0300

    peticion

--- react-ejericicios/src/Components/SongSearch.js
+++ react-ejericicios/src/Components/SongSearch.js
@@ -2,12 +2,40 @@ import React, { useState, useEffect } from "react";
 import SongForm from "./SongForm"
 import SongDetails from "./SongDetails";
 import Loader from "./Loader";
+import helHttp, { helpHttp } from "../helpers/helpHttp.js"
+
 
 const SongSearch = () => {
   const[search,setSearch]=useState(null)
   const [lyric, setLyric] = useState(null);
   const [bio, setBio] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  useEffect(()=>{
+    if(search === null) return
+
+    const fetchData = async () =>{
+        const {artist,song} = search;
+
+        let artistUrl = ` https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+        let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+        setLoading(true);
+       
+        const [artistRes,songRes] = await Promise.all([
+          helpHttp().get(artistUrl),
+          helpHttp().get(songUrl)
+        ]);
+
+        console.log(artistRes,songRes);
+
+        setBio(artistRes);
+        setLyric(songRes);
+        setLoading(false);
+    }
+
+    fetchData();
+  },[search])
 
   const handleSearch = (data)=>{
     //console.log(data)
