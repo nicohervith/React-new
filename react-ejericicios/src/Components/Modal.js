b165203a4962305ee3802a1c2890406b9b2eba1f
nicohervith <niko_hervith@hotmail.com>
2021-11-23 15:26:48 -0300

    window modal 3/3

--- react-ejericicios/src/Components/Modal.js
+++ react-ejericicios/src/Components/Modal.js
@@ -1,10 +1,14 @@
 import './Modal.css'
 
-const Modal = ({children}) => {
+const Modal = ({children, isOpen, closeModal}) => {
+  
+  //  ***************  IMPORTANTE *******************
+  //Con este stopPropagation evito que la imagen modal se cierre al hacerle click encima
+  const handleModalContainerClick = (e) => e.stopPropagation();
   return (
-    <article className="modal is-open">
-      <div className="modal-container">
-          <button className="modal-close">X</button>
+    <article className={`modal ${isOpen && "is-open"}`} onClick= {closeModal}>
+      <div className="modal-container" onClick={handleModalContainerClick}>
+          <button class="modal-close" onClick={closeModal}>X</button>
           {children}
       </div>
     </article>
--- react-ejericicios/src/Components/Modals.js
+++ react-ejericicios/src/Components/Modals.js
@@ -1,27 +1,48 @@
 import React from "react";
 import { useModal } from "../hooks/useModal";
 import Modal from "./Modal";
+import ContactForm from './ContactForm'
+import SongSearch from "./SongSearch";
 
 const Modals = ( ) => {
   const [isOpenModal1, openModal1,closeModal1] = useModal(false)
 
   const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+
+  const [isOpenContact, openModalContact, closeModalContact] = useModal(false);
+
+   const [isOpenSong, openModalSong, closeModalSong] = useModal(false);
   return (
     <div>
       <h2> Modales </h2>
-      <button> Modal 1 </button>
-      
+
+      <button onClick={openModal1} closeModal={closeModal1}>
+        Modal 1
+      </button>
       {/* Por cada modal que quiera controlar, utilizo la funcion creada en el useModal*/}
-      <Modal>
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
         <h3> Modal 1</h3>
         <p>Contenido de mi modal 1</p>
-        <img src="https://placeimg.com/400/400/animals" alt="animals" />
+        <img src="https://placeimg.com/600/600/animals" alt="animals" />
       </Modal>
-      <button> Modal 2</button>
-      <Modal>
+
+      <button onClick={openModal2} closeModal={closeModal2}>
+        Modal 2
+      </button>
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
         <h3> Otro modal</h3>
-        <p>Contenido de mi modal 1</p>
-        <img src="https://placeimg.com/400/400/nature" alt="nature" />
+        <p>Contenido de mi modal 2</p>
+        <img src="https://placeimg.com/600/600/nature" alt="nature" />
+      </Modal>
+
+      <button onClick={openModalContact}>Modal contacto</button>
+      <Modal isOpen={isOpenContact} closeModal={closeModalContact}>
+        <ContactForm />
+      </Modal>
+
+      <button onClick={openModalSong}>Modal Canciones</button>
+      <Modal isOpen={isOpenSong} closeModal={closeModalSong}>
+        <SongSearch />
       </Modal>
     </div>
   );
