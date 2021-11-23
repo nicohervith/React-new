1ce06643a7cf8913aa4c5b75f4616e892cea64e3
nicohervith <niko_hervith@hotmail.com>
2021-11-23 12:44:48 -0300

    ventana modal 2/3

--- react-ejericicios/src/Components/Modals.js
+++ react-ejericicios/src/Components/Modals.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useModal } from "../hooks/useModal";
 import Modal from "./Modal";
 
 const Modals = ( ) => {
+  const [isOpenModal1, openModal1,closeModal1] = useModal(false)
+
+  const [isOpenModal2, openModal2, closeModal2] = useModal(false);
   return (
     <div>
       <h2> Modales </h2>
       <button> Modal 1 </button>
+      
+      {/* Por cada modal que quiera controlar, utilizo la funcion creada en el useModal*/}
       <Modal>
         <h3> Modal 1</h3>
         <p>Contenido de mi modal 1</p>
