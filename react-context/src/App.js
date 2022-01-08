154a477928ff7fc764b670d117c033830df519ca
nicohervith <niko_hervith@hotmail.com>
2022-01-08 14:12:03 -0300

    update

---
+++ react-context/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import MyPage from './Components/MyPage';
+
+function App() {
+  return (
+    <div>
+      <h1>React Context</h1>
+              <a
+                href="https://es.reactjs.org/docs/context.html"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Documentación
+              </a>
+          <hr />
+          <MyPage/>
+    </div>
+  );
+}
+
+export default App;
