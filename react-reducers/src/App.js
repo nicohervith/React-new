9c4defb662e9ac794153b5823429b42f06352000
nicohervith <niko_hervith@hotmail.com>
2022-02-07 02:28:46 -0300

    react-reducers

---
+++ react-reducers/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Contador from './Components/Contador';
+
+function App() {
+  return (
+    <div>
+        <h1> React Reducers </h1>
+        <hr/>
+        <Contador/>
+    </div>
+  );
+}
+
+export default App;
