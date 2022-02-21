67b81ad402dad45d57bf41adfb12204811e0972a
nicohervith <niko_hervith@hotmail.com>
2022-02-21 11:56:05 -0300

    useReducer

--- react-reducers/src/App.js
+++ react-reducers/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Contador from './Components/Contador';
+import ContadorMejorado from './Components/ContadorMejorado';
 
 function App() {
   return (
     <div>
-        <h1> React Reducers </h1>
-        <hr/>
-        <Contador/>
+      <h1> React Reducers </h1>
+      <hr />
+      <ContadorMejorado/>
+      <hr />
+      <Contador />
     </div>
   );
 }
---
+++ react-reducers/src/Components/ContadorMejorado.js
@@ -0,0 +1,36 @@
+import React, { useReducer } from 'react';
+import { TYPES } from '../Actions/contadorActions';
+import { contadorInit, contadorInitialState, contadorReducer } from '../Reducers/contadorReducer';
+
+
+const ContadorMejorado = () => {
+
+  const [state,dispatch]=useReducer(contadorReducer, contadorInitialState, contadorInit)
+
+  const sumar= ()=>dispatch({type: TYPES.INCREMENT});
+
+   const sumar5 = () => dispatch({ type:TYPES.INCREMENT_5, payload:5 });
+
+  const restar = () => dispatch({type:TYPES.DECREMENT});
+
+    const restar5 = () => dispatch({ type: TYPES.DECREMENT_5, payload:5});
+
+    const reset = () => dispatch({ type: TYPES.RESET })
+
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Contador Mejorado Reducer</h2>
+      <nav>
+        <button onClick={sumar5}>+5</button>
+        <button onClick={sumar}>+</button>
+        <button onClick={reset}>0</button>
+        <button onClick={restar}>-</button>
+        <button onClick={restar5}>-5</button>
+      </nav>
+      <h3>{state.contador}</h3>
+    </div>
+  );
+};
+
+export default ContadorMejorado;
