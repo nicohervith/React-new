632d11f415b6ae159a35cbb9471611a98ead5f3c
nicohervith <niko_hervith@hotmail.com>
2022-02-22 10:27:40 -0300

    shopping cart

--- react-reducers/src/Components/ShoppingCart.js
+++ react-reducers/src/Components/ShoppingCart.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import {shoppingInitialState, shoppingReducer} from '../Reducers/shoppingReducer'
+import ProductItem from './ProductItem';
 
 const ShoppingCart = () => {
+
+  const[state,dispatch] = useReducer( shoppingReducer, shoppingInitialState);
+
+  const { products,cart }= state;
+
+  const addToCart = (id) =>{
+
+  }
+
+  const delFromCart = () =>{
+
+  }
+
+  const clearCart = () => {};
+
+
   return (
     <div>
       <h2>Carrito de Compras</h2>
       <h3> Productos</h3>
-      <article className="box">
-            
+      <article className="box grid-responsive">
+        {products.map((product) => (
+          <ProductItem key={product.id} data={product} addToCart={addToCart} />
+        ))}
       </article>
-        <h3> Carrito</h3>
-      <article className="box">
-      </article>
+      <h3> Carrito</h3>
+      <article className="box"></article>
     </div>
   );
 }
