489c05067bf536bdb05559bdcd862af2fc594a8d
nicohervith <niko_hervith@hotmail.com>
2022-03-02 13:14:58 -0300

    crud reducer

--- react-reducers/src/App.js
+++ react-reducers/src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Contador from './Components/Contador';
 import ContadorMejorado from './Components/ContadorMejorado';
 import ShoppingCart from './Components/ShoppingCart';
+import CrudApi from './Components/CrudApi';
 
 function App() {
   return (
     <div>
       <h1> React Reducers </h1>
+      <hr/>
+      <CrudApi/>
       <hr />
       <ShoppingCart/>
       <hr />
