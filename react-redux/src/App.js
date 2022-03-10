269a761714f60679a3edf370412238b51ce4bb44
nicohervith <niko_hervith@hotmail.com>
2022-03-10 12:28:54 -0300

    contador redux

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,4 +1,5 @@
 import { Provider } from 'react-redux';
+import Contador from './components/Contador';
 import TeoriaRedux from './components/TeoriaRedux';
 import store from './store';
 
@@ -8,6 +9,8 @@ function App() {
     <Provider store={store}>
       <div style={{ textAlign: "center" }}>
         <h1>Redux</h1>
+        <hr/>
+        <Contador/>
         <hr />
         <TeoriaRedux />
       </div>
